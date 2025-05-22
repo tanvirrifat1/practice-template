4322bceb5a518a9d5c34bb3c4f625406f4559dea
tanvirrifat1 <rifatkhan5567790@gmail.com>
2025-05-22 20:46:25 +0600

    added

---
+++ src/app/modules/room/room.model.ts
@@ -0,0 +1,17 @@
+import { model, Schema } from 'mongoose';
+import { IChatRoom } from './room.interface';
+
+const chatRoomSchema = new Schema<IChatRoom>(
+  {
+    roomName: {
+      type: String,
+      required: true,
+    },
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+    },
+  },
+  { timestamps: true }
+);
+export const ChatRoom = model<IChatRoom>('Room', chatRoomSchema);
